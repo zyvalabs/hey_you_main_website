7a9caacd86a7b74360d3c78e35e5819f27d538fd
Zyva Labs <zyvalabs@gmail.com>
2026-06-10 14:29:44 +0530

    commit

--- src/routes/drink-eat.tsx
+++ src/routes/drink-eat.tsx
@@ -1,8 +1,10 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { Header, BottomNav } from "@/components/Header";
-import drinkEatStaySvg from '../assets/heyou/drinkeatstay.png'
-import tongue from '../assets/heyou/tongue@2x.png'
-import barSide from '../assets/heyou/reserve table@2x.png'
+import drinkEatStaySvg from '../assets/heyou/mobile/drinkeatstay.svg'
+import greenSvg from '../assets/heyou/mobile/green.svg'
+import blueSvg from '../assets/heyou/mobile/blue.svg'
+import tongue1Svg from '../assets/heyou/mobile/toungue2.svg'
+import barSideSvg from '../assets/heyou/mobile/bar side.svg'
 import { Footer } from "@/components/Footer";
 import { Link } from "@tanstack/react-router";
 
@@ -20,14 +22,21 @@ function DrinkEat() {
   return (
     <div className="bg-[var(--hey-black)]">
       <Header active="/drink-eat" />
-      <img src={drinkEatStaySvg} alt="Drink Eat Stay" style={{ width: '100%', height: 'auto', display: 'block', margin: 0, padding: 0 }} />
-      <img src={tongue} alt="Tongue" style={{ width: '100%', height: 'auto', display: 'block', margin: 0, padding: 0 }} />
+      <img src={drinkEatStaySvg} alt="Drink Eat Stay" className="w-full h-auto block" />
 
-<Link to="/reserve">
-  <img src={barSide} alt="Bar Side" style={{ width: '100%', height: 'auto', display: 'block', margin: 0, padding: 0 }} />
-</Link>
+      {/* GREEN + BLUE */}
+      <div className="flex flex-col md:flex-row">
+        <div className="flex-1"><img src={greenSvg} alt="Green" className="w-full h-full object-cover" /></div>
+        <div className="flex-1"><img src={blueSvg} alt="Blue" className="w-full h-full object-cover" /></div>
+      </div>
 
-<Footer />
+      {/* TONGUE1 + BAR SIDE */}
+      <div className="flex flex-col md:flex-row">
+        <div className="flex-1"><img src={tongue1Svg} alt="Tongue" className="w-full h-full object-cover" /></div>
+        <div className="flex-1"><img src={barSideSvg} alt="Bar Side" className="w-full h-full object-cover" /></div>
+      </div>
+
+      <Footer />
       <BottomNav />
     </div>
   );
